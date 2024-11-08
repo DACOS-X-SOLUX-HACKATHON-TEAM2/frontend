1b85a0048325c62f452dad96fdece9ce37243454
namdaeun <nde40345@gmail.com>
2024-11-08 18:38:36 +0900

    feat: 버튼 공통 컴포넌트 생성

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+/** @jsxImportSource @emotion/react */
+
+import React, { HTMLAttributes } from "react";
+import { buttonStyle, variantStyle } from "./Button.style";
+
+export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  variant: "primary" | "secondary";
+}
+
+const Button = ({ variant = "primary", children, ...props }: ButtonProps) => {
+  return (
+    <button type="button" {...props} css={[buttonStyle, variantStyle(variant)]}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
